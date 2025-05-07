2f2815765cfb4043a883475944df44bd636bbb13
MoriDevoff <Bonustry@gmail.com>
2025-05-07 23:04:56 +0700

    pochinil

--- carshop/main/urls.py
+++ carshop/main/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import *
+from . import views
 
 urlpatterns = [
-    path('', home)
+    path('', views.car_list, name='car_list'),
+    path('car/<int:car_id>/', views.car_detail, name='car_detail'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('favorites/add/<int:car_id>/', views.add_to_favorites, name='add_to_favorites'),
+    path('favorites/', views.view_favorites, name='view_favorites'),
+    path('create-car/', views.create_car, name='create_car'),
 ]
