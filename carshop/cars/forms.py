6800ca23d737c8474107afb34ad8aafd7b041b70
MoriDevoff <Bonustry@gmail.com>
2025-05-08 20:12:52 +0700

    registr ready

--- carshop/cars/forms.py
+++ carshop/cars/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Car, CarSpecification
 
 class LoginForm(forms.Form):
@@ -10,10 +12,38 @@ class LoginForm(forms.Form):
         widget=forms.PasswordInput(attrs={'class': 'w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'})
     )
 
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(
+        max_length=254,
+        required=True,
+        help_text='Введите действительный адрес электронной почты.',
+        widget=forms.EmailInput(attrs={'class': 'w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'})
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'class': 'w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'
+        })
+        self.fields['email'].widget.attrs.update({
+            'class': 'w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'class': 'w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'class': 'w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'
+        })
+
 class CarForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = ['brand', 'model', 'year', 'price', 'mileage', 'engine_capacity', 'fuel_type', 'transmission', 'color', 'description', 'main_photo_url']
+
     def __init__(self, *args, **kwargs):
         super(CarForm, self).__init__(*args, **kwargs)
         for field in self.fields:
@@ -25,6 +55,7 @@ class CarSpecificationForm(forms.ModelForm):
     class Meta:
         model = CarSpecification
         fields = ['body_type', 'doors', 'seats', 'power', 'drive_type', 'vin', 'weight', 'country_of_origin']
+
     def __init__(self, *args, **kwargs):
         super(CarSpecificationForm, self).__init__(*args, **kwargs)
         for field in self.fields:
--- carshop/cars/views.py
+++ carshop/cars/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from .models import Car, CarSpecification, Favorite, User
-from .forms import LoginForm, CarForm, CarSpecificationForm
-from django.contrib.auth.hashers import make_password, check_password
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Car, CarSpecification, Favorite, PurchaseRequest
+from .forms import LoginForm, RegisterForm, CarForm, CarSpecificationForm
 
 def car_list(request):
-    cars = Car.objects.filter(is_sold=False)
+    cars = Car.objects.all()
     return render(request, 'car_list.html', {'cars': cars})
 
 def car_detail(request, car_id):
     car = get_object_or_404(Car, id=car_id)
-    specs = get_object_or_404(CarSpecification, car=car)
+    specs = CarSpecification.objects.filter(car=car).first()
     return render(request, 'car_detail.html', {'car': car, 'specs': specs})
 
 def user_login(request):
@@ -21,58 +22,92 @@ def user_login(request):
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            try:
-                user = User.objects.get(username=username)
-                if check_password(password, user.password_hash):
-                    request.session['user_id'] = user.id
-                    messages.success(request, 'Вы успешно вошли!')
-                    return redirect('car_list')
-                else:
-                    messages.error(request, 'Неверный пароль.')
-            except User.DoesNotExist:
-                messages.error(request, 'Пользователь не найден.')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                request.session['user_id'] = user.id
+                messages.success(request, 'Вы успешно вошли!')
+                return redirect('car_list')
+            else:
+                messages.error(request, 'Неверное имя пользователя или пароль.')
     else:
         form = LoginForm()
     return render(request, 'login.html', {'form': form})
 
+def user_register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, 'Регистрация прошла успешно! Теперь вы можете войти.')
+            return redirect('login')
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{field}: {error}")
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
 def user_logout(request):
-    if 'user_id' in request.session:
-        del request.session['user_id']
+    logout(request)
+    request.session.flush()
     messages.success(request, 'Вы успешно вышли!')
     return redirect('car_list')
 
-@login_required(login_url='/login/')
+@login_required
 def add_to_favorites(request, car_id):
     car = get_object_or_404(Car, id=car_id)
-    user_id = request.session.get('user_id')
-    user = get_object_or_404(User, id=user_id)
+    user = request.user
     Favorite.objects.get_or_create(user=user, car=car)
     messages.success(request, 'Автомобиль добавлен в избранное!')
-    return redirect('car_list')
+    return redirect('car_detail', car_id=car_id)
 
-@login_required(login_url='/login/')
+@login_required
+@csrf_exempt
+def remove_from_favorites(request, favorite_id):
+    if request.method == 'POST':
+        favorite = get_object_or_404(Favorite, id=favorite_id, user=request.user)
+        favorite.delete()
+        return JsonResponse({'success': True})
+    return JsonResponse({'success': False})
+
+@login_required
+def send_purchase_request(request, car_id):
+    car = get_object_or_404(Car, id=car_id)
+    user = request.user
+    existing_request = PurchaseRequest.objects.filter(user=user, car=car).first()
+    if existing_request:
+        messages.warning(request, 'Вы уже отправили запрос на этот автомобиль.')
+    else:
+        PurchaseRequest.objects.create(user=user, car=car)
+        messages.success(request, 'Запрос на покупку успешно отправлен!')
+    return redirect('car_detail', car_id=car_id)
+
+@login_required
 def view_favorites(request):
-    user_id = request.session.get('user_id')
-    user = get_object_or_404(User, id=user_id)
-    favorites = Favorite.objects.filter(user=user)
+    favorites = Favorite.objects.filter(user=request.user)
     return render(request, 'favorites.html', {'favorites': favorites})
 
-@login_required(login_url='/login/')
+@login_required
+def view_purchase_requests(request):
+    requests = PurchaseRequest.objects.filter(user=request.user)
+    return render(request, 'requests.html', {'requests': requests})
+
+@login_required
 def create_car(request):
     if request.method == 'POST':
         car_form = CarForm(request.POST)
         spec_form = CarSpecificationForm(request.POST)
         if car_form.is_valid() and spec_form.is_valid():
-            user_id = request.session.get('user_id')
-            user = get_object_or_404(User, id=user_id)
-            car = car_form.save(commit=False)
-            car.user = user
-            car.save()
+            car = car_form.save()
             spec = spec_form.save(commit=False)
             spec.car = car
             spec.save()
-            messages.success(request, 'Объявление успешно опубликовано!')
+            messages.success(request, 'Объявление успешно создано!')
             return redirect('car_list')
+        else:
+            messages.error(request, 'Пожалуйста, исправьте ошибки в форме.')
     else:
         car_form = CarForm()
         spec_form = CarSpecificationForm()
